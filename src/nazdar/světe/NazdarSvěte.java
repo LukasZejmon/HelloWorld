664be43da113fe2fd605fbadc52a5135b2890021
lukas.zejmon <lukas.zejmon@W10109.sps-cl.local>
2018-03-26 12:22:04 +0200

    prvni commit

---
+++ src/nazdar/světe/NazdarSvěte.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nazdar.světe;
+
+/**
+ *
+ * @author lukas.zejmon
+ */
+public class NazdarSvěte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nazdar bazar!");
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i + " ");
+        }
+    }
+
+}
